3c4e3223f6d344a83e7a0167f5498f1b6ce998c7
Devon <devon@artvuu.group>
2021-09-24 22:23:31 +0930

    updated boss attack controller

--- Assets/AttackControllerTheGolem.cs
+++ Assets/AttackControllerTheGolem.cs
@@ -24,11 +24,9 @@ public class AttackControllerTheGolem : MonoBehaviour
 
     private bool slammingHand;
 
-    private bool canFollow = true;
+    private bool canFollowPlayer = true;
 
     private GameObject player;
-
-
 
     void Start()
     {
@@ -44,42 +42,53 @@ public class AttackControllerTheGolem : MonoBehaviour
     {
         while (enabled)
         {
-            if (canFollow)
+            if (canFollowPlayer)
             {
-                float step = handFollowSpeed * Time.deltaTime;
-                leftHand.transform.position = Vector3.MoveTowards(leftHand.transform.position, new Vector3(player.transform.position.x, leftHand.transform.position.y), step);
+                moveHandToPlayer();
             }
             yield return null;
         }
     }
+    void moveHandToPlayer()
+    {
+        float step = handFollowSpeed * Time.deltaTime;
+        leftHand.transform.position = Vector3.MoveTowards(leftHand.transform.position, new Vector3(player.transform.position.x, leftHand.transform.position.y), step);
+
+    }
     IEnumerator fistSlam()
     {
         slamHand();
-        canFollow = false;
+        canFollowPlayer = false;
         yield return new WaitForSeconds(5f);
         initialHandPosition.x = leftHand.transform.position.x;
         StartCoroutine("raiseHand");
-    }
-
-    IEnumerator raiseHand()
-    {
-        while (enabled)
-        {
-            float step = handRaiseSpeed * Time.deltaTime;
-            leftHand.transform.position = Vector3.MoveTowards(leftHand.transform.position, initialHandPosition, step);
-            if (leftHand.transform.position.y == initialHandPosition.y && leftHand.transform.position.x == initialHandPosition.x)
-            {
-                canFollow = true;
-                yield return new WaitForSeconds(2.5f);
-                StartCoroutine("fistSlam");
-                StopCoroutine("raiseHand");
-            }
-            yield return new WaitForSeconds(0f);
-        }
     }
 
     void slamHand()
     {
         leftHandRb.AddForce(Vector2.down * slammingForce, ForceMode2D.Impulse);
     }
+
+    IEnumerator raiseHand()
+    {
+        while (enabled)
+        {
+            moveHandToInitialSlamPosition();
+            if (leftHand.transform.position.y == initialHandPosition.y && leftHand.transform.position.x == initialHandPosition.x)
+            {
+                canFollowPlayer = true;
+                yield return new WaitForSeconds(2.5f);
+                StopCoroutine("raiseHand");
+                StartCoroutine("fistSlam");
+            }
+            yield return new WaitForSeconds(0f);
+        }
+    }
+
+    void moveHandToInitialSlamPosition()
+    {
+        float step = handRaiseSpeed * Time.deltaTime;
+        leftHand.transform.position = Vector3.MoveTowards(leftHand.transform.position, initialHandPosition, step);
+    }
+
 }
