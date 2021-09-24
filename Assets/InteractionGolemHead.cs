87d7025024e4deac8a1f5f7e40168d75e0caed1e
Devon <devon@artvuu.group>
2021-09-25 04:30:18 +0930

    added golem head recoil

--- Assets/InteractionGolemHead.cs
+++ Assets/InteractionGolemHead.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class InteractionGolemHead : MonoBehaviour
 {
 
+    [SerializeField] int recoilForce = 65;
     private BossInteractionTheGolem mainInteractionScript;
     void Start()
     {
@@ -15,7 +16,16 @@ public class InteractionGolemHead : MonoBehaviour
     {
         if (col.collider.tag == "Sword" && mainInteractionScript.canTakeDamage == true)
         {
+            golemHeadRecoil(col.gameObject);
             mainInteractionScript.golemHeadHit();
         }
+    }
+
+    void golemHeadRecoil(GameObject colider)
+    {
+        Rigidbody2D rb = colider.GetComponent<Rigidbody2D>();
+
+        rb.AddForce(Vector2.left * recoilForce, ForceMode2D.Impulse);
+        rb.AddForce(Vector2.up * (recoilForce / 2), ForceMode2D.Impulse);
     }
 }
