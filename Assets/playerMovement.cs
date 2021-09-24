6123e10f7dc4b5e864c5f4dfdf4bf807ca7102e1
Devon <devon@artvuu.group>
2021-09-24 20:31:25 +0930

    implemented dashing

--- Assets/playerMovement.cs
+++ Assets/playerMovement.cs
@@ -4,15 +4,79 @@ using UnityEngine;
 
 public class playerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField]
+    private float horizontalSpeed;
+    [SerializeField]
+    private float dashSpeed = 150F;
+    private Animator playerAnimator;
+
+    [SerializeField]
+    private GameObject playerBottom;
+
+    private bool moving;
+
+    private Rigidbody2D rb;
+
     void Start()
     {
-        
+        playerAnimator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
+    void LateUpdate()
+    {
+        playerAnimator.SetBool("isMoving", moving);
+    }
+
     void Update()
     {
-        
+
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            dashPlayer(0);
+        }
+        else if (Input.GetKeyDown(KeyCode.E))
+        {
+            dashPlayer(1);
+        }
+
+    }
+
+    void FixedUpdate()
+    {
+        float horizontalMovement = Input.GetAxis("HorizontalMoving");
+        if (horizontalMovement != 0)
+        {
+            movePlayerHorizontally(horizontalMovement);
+        }
+        isPlayerMoving(horizontalMovement);
+    }
+
+    void isPlayerMoving(float horizontalMovement)
+    {
+        moving = horizontalMovement != 0;
+    }
+
+    void dashPlayer(int dashingDirection)
+    {
+        Vector2 dashingPosition = dashingDirection == 0 ? Vector2.left : Vector2.right;
+        rb.AddForce(dashSpeed * dashingPosition, ForceMode2D.Impulse);
+    }
+
+
+    void movePlayerHorizontally(float horizontalMovement)
+    {
+        updatePlayerDirection(horizontalMovement);
+
+        Vector2 horizontalDirection =
+            horizontalMovement <= 0 ? Vector2.left : Vector2.right;
+        transform
+            .Translate(horizontalDirection * Time.deltaTime * horizontalSpeed);
+    }
+
+    void updatePlayerDirection(float horizontalMovement)
+    {
+        playerBottom.transform.localScale =
+            horizontalMovement >= 0 ? new Vector2(-1, 1) : new Vector2(1, 1);
     }
 }
