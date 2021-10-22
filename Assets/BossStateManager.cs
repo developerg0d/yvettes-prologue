19ed126136c245279956372f6f49ec07611f2f2f
Devon <devon@artvuu.group>
2021-10-23 00:30:32 +1030

    added initial boss stage state manager

--- Assets/BossStateManager.cs
+++ Assets/BossStateManager.cs
@@ -1,18 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEditor;
+using UnityEngine.Serialization;
 
 public class BossStateManager : MonoBehaviour
 {
+    [Header("Boss State Manager")]
+    [Space(10)]
+
+    [Tooltip("This script allows you to set an array of Boss Stage Events.This will allow you to customize the Boss Event Lifecycle for each boss how you please.")]
+    public UnityEvent[] bossStageEvents;
+
+    public int currentStage;
 
     void Start()
     {
-
+        UpdateBossStage();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void NextBossStage()
     {
-
+        currentStage += 1;
+        bossStageEvents[currentStage].Invoke();
     }
+    private void UpdateBossStage()
+    {
+        bossStageEvents[currentStage].Invoke();
+    }
+
 }
